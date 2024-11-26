47625b14f787e5469664897dd653020af4bd5de8
Meroshanc <bumer2116@yandex.com>
2024-11-26 11:11:43 +0300

    Add files via upload

---
+++ homework1_11.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+namespace part01 
+{
+    void callpart01()
+    {
+        std::string firstName, surName;
+        std::cout << "Введите имя: ";
+        std::cin >> firstName;
+        std::cout << "\nВведите фамилию: ";
+        std::cin >> surName;
+        std::string greeting = "Здравствуйте, " + firstName + " " + surName + "!";
+        std::cout << greeting;
+    }
+}
+
+namespace part02
+{
+    void callpart02()
+    {
+        std::string hiddenWord = "строка";
+        std::string guessing;
+        while (1)
+        {
+            std::cout << "Угадайте слово: ";
+            std::cin >> guessing;
+            if (guessing == hiddenWord)
+            {
+                std::cout << "Правильно! Вы победили! Загаданное слово - " << guessing;
+                return;
+            }
+            else
+                std::cout << "Неправильно" << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "Russian");
+    part01::callpart01();
+    part02::callpart02();
+}
